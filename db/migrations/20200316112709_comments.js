1ed0b732bd9f93d0e0e0e068db12e8e82e9be6ce
xfiddyx <steven.woods88@hotmail.co.uk>
2020-03-25 08:12:38 +0000

    minor amends

--- db/migrations/20200316112709_comments.js
+++ db/migrations/20200316112709_comments.js
@@ -8,7 +8,10 @@ exports.up = function(knex) {
       .string('author')
       .references('users.username')
       .notNullable();
-    commentsTable.integer('article_id').references('articles.article_id');
+    commentsTable
+      .integer('article_id')
+      .references('articles.article_id')
+      .onDelete('CASCADE');
     commentsTable.integer('votes').defaultTo(0);
     commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
     commentsTable.string('body', 1500).notNullable();
