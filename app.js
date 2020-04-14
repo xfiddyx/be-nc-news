6736b12f77b9c3f4de309f376862e8f4c60162b4
xfiddyx <steven.woods88@hotmail.co.uk>
2020-04-14 08:58:43 +0100

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const apiRouter = require('./routes/api.router');
 const { statusCodeErrors } = require('./errors/app.errors');
 const { psqlErrors } = require('./errors/psql.errors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
